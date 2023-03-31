be7133a444e2b04d7c922fc3acb4026853119fad
KaranjaRic <karanja.erick04@gmail.com>
2023-03-31 12:28:56 -0700

    return number of elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * list_len - Checks code for Holberton School students
+ * @h: Name of the list
+ * Return: The number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
