807da3efe2d09dcf73d773adbcc1252fd1905910
KaranjaRic <karanja.erick04@gmail.com>
2023-03-31 13:49:02 -0700

    print a function before main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * myStartupFun - Apply the constructor attribute to myStartupFun()
+ * so it is executed before main()
+ */
+void myStartupFun(void) __attribute__ ((constructor));
+
+ /**
+  * myStartupFun - Implementation of myStartupFun
+  */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you msut allow,\n");
+	printf("I bore my house upon my back!\n");
+}
